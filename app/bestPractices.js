2d33ef8c2497f862d279843786016a817ccdaa3d
danrley.teixeira <danrley.teixeira@dataprev.gov.br>
2019-05-28 15:18:44 -0300

    finishing bestpractices and flowcontrol

--- app/bestPractices.js
+++ app/bestPractices.js
@@ -1,4 +1,6 @@
-if (typeof define !== 'function') { var define = require('amdefine')(module); }
+if (typeof define !== 'function') {
+  var define = require('amdefine')(module);
+}
 
 /**
  * This file defines an object with some methods. Some of these methods are
@@ -7,30 +9,37 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
  */
 define(function() {
   return {
-    globals : function() {
-      myObject = {
-        name : 'Jory'
+    globals: function() {
+      var myObject = {
+        name: 'Jory'
       };
 
       return myObject;
     },
 
-    functions : function(flag) {
+    functions: function(flag) {
       if (flag) {
-        function getValue() { return 'a'; }
+        function getValue() {
+          return 'a';
+        }
       } else {
-        function getValue() { return 'b'; }
+        function getValue() {
+          return 'b';
+        }
       }
 
       return getValue();
     },
 
-    parseInt : function(num) {
-      return parseInt(num);
+    parseInt: function(num) {
+      return parseInt(num, 10);
     },
 
-    identity : function(val1, val2) {
-
+    identity: function(val1, val2) {
+      if (val1 === val2) {
+        return true;
+      }
+      return false;
     }
   };
 });
--- app/flowControl.js
+++ app/flowControl.js
@@ -1,16 +1,31 @@
-if (typeof define !== 'function') { var define = require('amdefine')(module); }
+if (typeof define !== 'function') {
+  var define = require('amdefine')(module);
+}
 
 define(function() {
+  // write a function that receives a number as its argument;
+  // if the number is divisible by 3, the function should return 'fizz';
+  // if the number is divisible by 5, the function should return 'buzz';
+  // if the number is divisible by 3 and 5, the function should return
+  // 'fizzbuzz';
+  //
+  // otherwise the function should return the number, or false if no number
+  // was provided or the value provided is not a number
   return {
-    fizzBuzz : function(num) {
-      // write a function that receives a number as its argument;
-      // if the number is divisible by 3, the function should return 'fizz';
-      // if the number is divisible by 5, the function should return 'buzz';
-      // if the number is divisible by 3 and 5, the function should return
-      // 'fizzbuzz';
-      //
-      // otherwise the function should return the number, or false if no number
-      // was provided or the value provided is not a number
+    fizzBuzz: function(num) {
+      if (parseInt(num, 10)) {
+        if (num % 15 === 0) {
+          return 'fizzbuzz';
+        } else if (num % 3 === 0) {
+          return 'fizz';
+        } else if (num % 5 === 0) {
+          return 'buzz';
+        } else {
+          return num;
+        }
+      } else {
+        return false;
+      }
     }
   };
 });
