4a49cb9c5018d729fea5c1aa10ea715995db6023
danrley.teixeira <danrley.teixeira@dataprev.gov.br>
2019-05-28 15:40:38 -0300

    adding alter context

--- app/objects.js
+++ app/objects.js
@@ -1,17 +1,18 @@
-if (typeof define !== 'function') { var define = require('amdefine')(module); }
+if (typeof define !== 'function') {
+  var define = require('amdefine')(module);
+}
 
 define(function() {
   return {
-    alterContext : function(fn, obj) {
-
+    alterContext: function(fn, obj) {
+      this.name = obj.name;
+      this.greeting = obj.greeting;
+      obj.sayIt = fn;
+      return obj.sayIt();
     },
 
-    alterObjects : function(constructor, greeting) {
+    alterObjects: function(constructor, greeting) {},
 
-    },
-
-    iterate : function(obj) {
-
-    }
+    iterate: function(obj) {}
   };
 });
